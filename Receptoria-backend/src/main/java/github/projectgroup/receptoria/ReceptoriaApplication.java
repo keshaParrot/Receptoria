eaf89dda69373f9bf2b5ed8e191c04e288df696b
keshaParrot <strelok1323@gmail.com>
2025-04-14 15:40:56 +0200

    initial commit: added backend module

---
+++ Receptoria-backend/src/main/java/github/projectgroup/receptoria/ReceptoriaApplication.java
@@ -0,0 +1,13 @@
+package github.projectgroup.receptoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptoriaApplication.class, args);
+    }
+
+}
---
+++ Receptoria-backend/src/test/java/github/projectgroup/receptoria/ReceptoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package github.projectgroup.receptoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
