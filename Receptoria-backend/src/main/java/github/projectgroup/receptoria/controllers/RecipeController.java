544ce03c3a110fbe0839638d20f0495b69822c44
keshaParrot <strelok1323@gmail.com>
2025-06-04 14:54:28 +0200

    recipe update
    changed recipe url mapping

--- Receptoria-backend/src/main/java/github/projectgroup/receptoria/controllers/RecipeController.java
+++ Receptoria-backend/src/main/java/github/projectgroup/receptoria/controllers/RecipeController.java
@@ -8,27 +8,22 @@ import github.projectgroup.receptoria.domain.enities.UserRecipe;
 import github.projectgroup.receptoria.domain.mappers.ResultMapper;
 import github.projectgroup.receptoria.services.interfaces.RecipeService;
 
-import github.projectgroup.receptoria.utils.result.Result;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/recipes")
+@RequestMapping("/api/v1/recipes")
 @RequiredArgsConstructor
 public class RecipeController {
 
